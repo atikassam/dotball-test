fa2d62616068359f5db5bbed506ee8588cad934d
Atik <atikur@whyable.com>
2019-01-09 18:10:43 +0530

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+// let fields = Object.keys({
+//     "Title": "Ra.One",
+//     "Year": "2011",
+//     "Rated": "NOT RATED",
+//     "Released": "26 Oct 2011",
+//     "Runtime": "156 min",
+//     "Genre": "Action, Adventure, Sci-Fi",
+//     "Director": "Anubhav Sinha",
+//     "Writer": "David Benullo, Kanika Dhillon (dialogue), Kanika Dhillon (screenplay), Niranjan Iyengar (dialogue), Shah Rukh Khan (screenplay), Mushtaq Sheikh (screenplay), Anubhav Sinha (story), Anubhav Sinha, Rajshri Sudhakar (Dialogue Writer: Telugu)",
+//     "Actors": "Shah Rukh Khan, Arjun Rampal, Kareena Kapoor, Shahana Goswami",
+//     "Plot": "A computer game character takes on physical form to destroy a player who nearly beat him but quit the game in between.",
+//     "Language": "Hindi, Tamil, Telugu, Chinese, English",
+//     "Country": "India, USA",
+//     "Awards": "10 wins & 6 nominations.",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BMzcyMjMxOTg4MF5BMl5BanBnXkFtZTcwNzEwMDE5Ng@@._V1_SX300.jpg",
+//     "Ratings": [{"Source": "Internet Movie Database", "Value": "4.8/10"}, {
+//         "Source": "Rotten Tomatoes",
+//         "Value": "64%"
+//     }, {"Source": "Metacritic", "Value": "60/100"}],
+//     "Metascore": "60",
+//     "imdbRating": "4.8",
+//     "imdbVotes": "34,825",
+//     "imdbID": "tt1562871",
+//     "Type": "movie",
+//     "DVD": "N/A",
+//     "BoxOffice": "$2,509,150",
+//     "Production": "Eros Entertainment",
+//     "Website": "http://www.raonemovie.com/",
+//     "Response": "True"
+// });
+//
+//
+// let schema = {};
+//
+// fields.forEach(f => schema[f] = "String");
+//
+// console.log(schema);
